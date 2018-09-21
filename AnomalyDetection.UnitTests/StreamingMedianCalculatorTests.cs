472b3c88a0252208bd278c9d702a03268f494070
Adam Minchenton <adam.minchenton@sixpivot.com.au>
2018-09-22 06:45:25 +1000

    Add additional test cases, implement better solution for streaming median using Priority Queue

--- AnomalyDetection.UnitTests/StreamingMedianCalculatorTests.cs
+++ AnomalyDetection.UnitTests/StreamingMedianCalculatorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace AnomalyDetection.UnitTests
@@ -6,10 +7,10 @@ namespace AnomalyDetection.UnitTests
     public class StreamingMedianCalculatorTests
     {
         [Fact]
-        public void GivenAnEmptyStateAddANewNumberAndMedianIsThatNumber()
+        public void GivenAnEmptyState_WhenASingleNumberIsPushed_ThenTheMedianIsThatNumber()
         {
             var sut = new StreamingMedianCalculator();
-            var onlyNumber = 5;
+            var onlyNumber = 5.0;
 
             sut.Push(onlyNumber);
 
@@ -17,16 +18,102 @@ namespace AnomalyDetection.UnitTests
         }
 
         [Fact]
-        public void GivenAnEmptyStateAddTwoNewNumbersAndMedianIsAverageOfThoseNumbers()
+        public void GivenAnEmptyState_WhenTwoNumbersArePushed_ThenTheMedianIsAverageOfThoseNumbers()
         {
             var sut = new StreamingMedianCalculator();
-            var firstNumber = 5;
-            var secondNumber = 15;
+            var firstNumber = 5.0;
+            var secondNumber = 15.0;
 
             sut.Push(firstNumber);
             sut.Push(secondNumber);
 
-            var expected = (firstNumber + secondNumber) / 2;
+            var expected = (firstNumber + secondNumber) / 2.0;
+
+            Assert.Equal(expected, sut.Median());
+        }
+
+        [Fact]
+        public void GivenAnEmptyState_WhenThreeNumbersArePushed_AndTheyAreInOrder_ThenTheMedianIsTheSecondNumber()
+        {
+            var sut = new StreamingMedianCalculator();
+            var firstNumber = 5.0;
+            var secondNumber = 15.0;
+            var thirdNumber = 60.0;
+
+            sut.Push(firstNumber);
+            sut.Push(secondNumber);
+            sut.Push(thirdNumber);
+
+            Assert.Equal(secondNumber, sut.Median());
+        }
+        
+        [Fact]
+        public void GivenAnEmptyState_WhenFourNumbersArePushed_AndTheyAreInOrder_ThenTheMedianIsTheAverageOfTheSecondAndThirdNumbers()
+        {
+            var sut = new StreamingMedianCalculator();
+            var firstNumber = 5.0;
+            var secondNumber = 15.0;
+            var thirdNumber = 60.0;
+            var forthNumber = 74.0;
+
+            sut.Push(firstNumber);
+            sut.Push(secondNumber);
+            sut.Push(thirdNumber);
+            sut.Push(forthNumber);
+
+            var expected = (secondNumber + thirdNumber) / 2.0;
+
+            Assert.Equal(expected, sut.Median());
+        }
+        
+        [Fact]
+        public void GivenAnEmptyState_WhenThreeNumbersArePushed_AndTheyAreNotInOrder_ThenTheMedianIsTheSecondNumber()
+        {
+            var sut = new StreamingMedianCalculator();
+            var lowestNumber = 5.0;
+            var middleNumber = 15.0;
+            var highestNumber = 60.0;
+
+            sut.Push(middleNumber);
+            sut.Push(highestNumber);
+            sut.Push(lowestNumber);
+
+            Assert.Equal(middleNumber, sut.Median());
+        }
+        
+        [Fact]
+        public void GivenAnEmptyState_WhenFourNumbersArePushed_AndTheyAreNotInOrder_ThenTheMedianIsTheAverageOfTheSortedSecondAndThirdNumbers()
+        {
+            var sut = new StreamingMedianCalculator();
+            var lowestNumber = 5.0;
+            var secondLowestNumber = 15.0;
+            var secondHigestNumber = 60.0;
+            var highestNumber = 74.0;
+
+            sut.Push(secondLowestNumber);
+            sut.Push(lowestNumber);
+            sut.Push(secondHigestNumber);
+            sut.Push(highestNumber);
+
+            var expected = (secondLowestNumber + secondHigestNumber) / 2.0;
+
+            Assert.Equal(expected, sut.Median());
+        }
+
+        [Fact]
+        public void GivenAnEmptyState_WhenFourNumbersArePushed_AndTheFirstTwoNumbersAreEqual_ThenTheMedianIsTheAverageOfTheSortedSecondAndThirdNumbers() {
+            var sut = new StreamingMedianCalculator();
+            var lowestNumber = 5.0;
+            var secondLowestNumber = 5.0;
+            var secondHigestNumber = 60.0;
+            var highestNumber = 74.0;
+
+            sut.Push(secondLowestNumber);
+            sut.Push(lowestNumber);
+            sut.Push(secondHigestNumber);
+            sut.Push(highestNumber);
+
+            var expected = (secondLowestNumber + secondHigestNumber) / 2.0;
 
             Assert.Equal(expected, sut.Median());
         }
--- AnomalyDetection/SteamingMedianCalculator.cs
+++ AnomalyDetection/SteamingMedianCalculator.cs
@@ -1,3 +1,4 @@
+using SD.Tools.Algorithmia.PriorityQueues;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,20 +7,72 @@ namespace AnomalyDetection {
 
     public class StreamingMedianCalculator
     {
-        private List<double> numbers;
+        private SimplePriorityQueue<double> lowerTerms;
+        private SimplePriorityQueue<double> upperTerms;
 
         public StreamingMedianCalculator() {
-            this.numbers = new List<double>();
+            this.lowerTerms = new SimplePriorityQueue<double>((x,y) => x.CompareTo(y));
+            this.upperTerms = new SimplePriorityQueue<double>((x,y) => x.CompareTo(y) * -1);
         }
 
         public void Push(double nextNumber)
         {
-            numbers.Add(nextNumber);
+            PushToAppropriateSet(nextNumber);
+            RebalanceSets();
         }
 
         public double Median()
         {
-            return numbers.Average();
+            if (IsEvenTermList()) {
+                return GetMedianForEvenTermCountList();
+            }
+            return GetMedianForOddTermCountList();
+        }
+
+        private void PushToAppropriateSet(double term) {
+            var shouldAddToUpperTerms = term > lowerTerms.Peek();
+
+            if (shouldAddToUpperTerms) {
+                upperTerms.Add(term);
+            } else {
+                lowerTerms.Add(term);
+            }
+        }
+
+        private void RebalanceSets() {
+            RebalanceLowerTerms();
+            RebalanceUpperTerms();
+        }
+
+        private void RebalanceLowerTerms() {
+            var hasTooManyTerms = lowerTerms.Count - upperTerms.Count >= 2;
+
+            if (hasTooManyTerms) {
+                upperTerms.Add(lowerTerms.Peek());
+                lowerTerms.Remove();
+            }
+        }
+
+        private void RebalanceUpperTerms() {
+            var hasTooManyTerms = upperTerms.Count - lowerTerms.Count >= 1;
+
+            if (hasTooManyTerms) {
+                lowerTerms.Add(upperTerms.Peek());
+                upperTerms.Remove();
+            }
+        }
+
+        private bool IsEvenTermList() {
+            var totalCount = lowerTerms.Count + upperTerms.Count;
+            return totalCount % 2 == 0;
+        }
+
+        private double GetMedianForOddTermCountList() {
+            return lowerTerms.Peek();
+        }
+
+        private double GetMedianForEvenTermCountList() {
+            return (lowerTerms.Peek() + upperTerms.Peek()) / 2.0;
         }
     }
 }
