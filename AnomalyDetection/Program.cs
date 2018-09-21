938b405b75e5735cdf96fbe1b57e9fccefd649b4
Adam Minchenton <adam.minchenton@sixpivot.com.au>
2018-09-21 15:05:51 +1000

    Add new Project

---
+++ AnomalyDetection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnomalyDetection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
